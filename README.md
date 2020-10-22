cb4f17de308d42b461039459db08572b161edbb1
lminhtam <lminhtam99@gmail.com>
2020-10-23 03:17:21 +0700

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-## Tam Luong's Profile Web
-
-You can visit this website at https://www.lminhtam.github.io
---
+++ precache-manifest.d88ca61a9608a8ab980c1e09306f0ad8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df439d2f6cb79a4a1a0065aa20218321",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a097c95c5043640c0a2",
+    "url": "/static/css/main.635933c5.chunk.css"
+  },
+  {
+    "revision": "8645e7b0fe61f019f38f",
+    "url": "/static/js/2.cdbd13d0.chunk.js"
+  },
+  {
+    "revision": "2610ae67f72c897b371de52ff7224785",
+    "url": "/static/js/2.cdbd13d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a097c95c5043640c0a2",
+    "url": "/static/js/main.cd731ffa.chunk.js"
+  },
+  {
+    "revision": "99a3d2817b1f43b2b164",
+    "url": "/static/js/runtime-main.99ff291e.js"
+  },
+  {
+    "revision": "402427ce202075c8e249810378266833",
+    "url": "/static/media/CV_Luong_Minh_Tam.402427ce.pdf"
+  },
+  {
+    "revision": "f17716eccc7558ffcb47f2ed36a4c56c",
+    "url": "/static/media/avatar.f17716ec.jpg"
+  },
+  {
+    "revision": "0cb5a02aa353f3d928b8cf17757670ed",
+    "url": "/static/media/bg.0cb5a02a.jpg"
+  },
+  {
+    "revision": "33b40a836c96dd9b7dacfeb0dafe8348",
+    "url": "/static/media/big_avatar.33b40a83.jpg"
+  },
+  {
+    "revision": "0acc733e711c4fa876d2254356e8faf2",
+    "url": "/static/media/dimo.0acc733e.svg"
+  },
+  {
+    "revision": "581fcb2a3869b8cd25ec8ea2a7a54d9c",
+    "url": "/static/media/lemon-aid.581fcb2a.svg"
+  },
+  {
+    "revision": "41a69370cb4336e3ebdda169462ac8d9",
+    "url": "/static/media/tickit.41a69370.svg"
+  }
+]);
